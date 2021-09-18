03de527f0f404d99ff4beb4b38bb8f4bedcb94fd
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-09-18 16:16:38 -0400

    style edit blog handlebars

--- public/javascript/edit-blog.js
+++ public/javascript/edit-blog.js
@@ -2,13 +2,15 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="blog-title"]').value.trim();
+  const text = document.querySelector('textarea[name="blog-text"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
   const response = await fetch(`/api/blogs/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      title
+      title,
+      text
     }),
     headers: {
       'Content-Type': 'application/json'
@@ -22,4 +24,4 @@ async function editFormHandler(event) {
   }
 }
 
-document.querySelector('.edit-blogform').addEventListener('submit', editFormHandler);
+document.querySelector('.edit-blog-form').addEventListener('submit', editFormHandler);
